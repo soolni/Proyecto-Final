97f62d90e4f8fab864ef2703c36954ab45bfc420
BryanBarbie <bryan.sdp280944@gmail.com>
2023-08-29 08:33:53 -0300

    optimizacion de funcion

--- js/products.js
+++ js/products.js
@@ -1,21 +1,22 @@
 const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 const container = document.getElementById("products");
 
+
 function showDataProduct(dataArray) {
-    for (let item of dataArray) {
-      let itemHtml = '<div class="item">';
-      itemHtml += `<img src="${item.image}">`; 
-      itemHtml += '<div class="details">';
-      itemHtml += `ID: ${item.id}<br>`;
-      itemHtml += `Name: ${item.name}<br>`;
-      itemHtml += `Description: ${item.description}<br>`;
-      itemHtml += `Cost: ${item.cost} ${item.currency}<br>`;
-      itemHtml += `Sold Count: ${item.soldCount}<br>`;
-      itemHtml += '</div>';
-      itemHtml += '</div>';
-      container.innerHTML += itemHtml;
+  for (let item of dataArray) {
+    let itemHtml = '<div class="item">';
+    itemHtml += `<img src="${item.image}" class="image">`;
+    itemHtml += '<div class="details">';
+    for (let x in item) {
+      if (x !== 'image') {
+        itemHtml += `<div>${x}: ${item[x]}</div>`;
+      }
     }
+    itemHtml += '</div>';
+    itemHtml += '</div>';
+    container.innerHTML += itemHtml;
   }
+}
 
 
 fetch(DATA_URL)
