089eccf4cd1c77c3a9d2da4c761dfdb8a385a390
emigon <emipastor2001@gmail.com>
2023-08-31 08:44:20 -0300

    Pauta 2 entrega 2

--- js/products.js
+++ js/products.js
@@ -1,26 +1,22 @@
-const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${localStorage.getItem("catID")}.json`;
 const container = document.getElementById("products");
 
-function showDataProduct(dataArray) {
-    for (let item of dataArray) {
-      let itemHtml = '<div class="item">';
-      itemHtml += `<img src="${item.image}">`; 
-      itemHtml += '<div class="details">';
-      itemHtml += `ID: ${item.id}<br>`;
-      itemHtml += `Name: ${item.name}<br>`;
-      itemHtml += `Description: ${item.description}<br>`;
-      itemHtml += `Cost: ${item.cost} ${item.currency}<br>`;
-      itemHtml += `Sold Count: ${item.soldCount}<br>`;
-      itemHtml += '</div>';
-      itemHtml += '</div>';
-      container.innerHTML += itemHtml;
-    }
-  }
-
-
-fetch(DATA_URL)
-    .then(response =>  response.json())
-    .then (data => {showDataProduct(data.products)});
-    
-
-    
+  function showDataProduct(dataArray) {
+        for (let item of dataArray) {
+          let itemHtml = '<div class="item">';
+          itemHtml += `<img src="${item.image}">`; 
+          itemHtml += '<div class="details">';
+          itemHtml += `ID: ${item.id}<br>`;
+          itemHtml += `Nombre: ${item.name}<br>`;
+          itemHtml += `Descripción: ${item.description}<br>`;
+          itemHtml += `Precio: ${item.cost} ${item.currency}<br>`;
+          itemHtml += `Unidades vendidas: ${item.soldCount}<br>`;
+          itemHtml += '</div>';
+          itemHtml += '</div>';
+          container.innerHTML += itemHtml;
+        }
+      }
+    fetch(DATA_URL)
+        .then(response =>  response.json())
+        .then (data => {showDataProduct(data.products)}); 
+        
