1e54451a7156b07d6af61b09985e9bd498cf7847
emigon <emipastor2001@gmail.com>
2023-10-05 11:20:07 -0300

    Intento Desafiate

--- js/cart.js
+++ js/cart.js
@@ -26,23 +26,38 @@ function actualizarSubtotal() {
 document.querySelector('#inp').addEventListener('input', actualizarSubtotal);
 actualizarSubtotal();
 
+
+
+})
+
+
+
 const productosCarrito = JSON.parse(localStorage.getItem('productosCarrito'))
 
 console.log(productosCarrito)
 
 productosCarrito.forEach(producto => {
-    contenedor.innerHTML +=`
+    const container = document.querySelector('#carrito');
+    container.innerHTML +=`
     <div class="row">
     <div class="col"><img class="" src="${producto.image}" style="width: 4rem"></div>
     <div class="col">${producto.name}</div>
     <div class="col">USD ${producto.cost}</div>
     <div class="col"><input id="${producto.id}" type="number" min="0" style="width:60px" value="${producto.count}"></div>
-    <div class="A${producto.id} col">${producto.currency} ${producto.cost * producto.count}</div>
+    <div class="A${producto.id} col"></div>
     <div class="col"></div>
 </div>
     `
+    const cantidadLocal = document.getElementById(producto.id);
+    cantidadLocal.addEventListener('input', actualizarSubtotalLocal);
+
+   
+    function actualizarSubtotalLocal() {
+        const cantidad = cantidadLocal.value;
+        const subtotal = cantidad * producto.cost;
+        const subtotalElement = document.querySelector(`.A${producto.id}`);
+        subtotalElement.textContent = `USD ${subtotal}`;
+    }
+    document.querySelectorAll(`'${producto.id}'`).addEventListener('input', actualizarSubtotalLocal);
+    actualizarSubtotalLocal();
 });
-})
-
-
-
