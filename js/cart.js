93156ce0198ef95d97fa8c5b1da8ffd6969be484
Israel Buscarons <israelbuscarons@gmail.com>
2023-10-14 15:20:22 -0300

    Poner global constante contenedor

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,9 @@
 const URL_carrito = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
+const contenedor = document.querySelector('#carrito');
+
 fetch(URL_carrito)
 .then(response => response.json())
 .then(data => {
-const contenedor = document.querySelector('#carrito');
 contenedor.innerHTML += `
 <div class="row">
     <div class="col"><img class="" src="${data.articles[0].image}" style="width: 4rem"></div>
