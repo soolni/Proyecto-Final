14382425ff61f3f380467bfa35643b4cc072dee0
Israel Buscarons <israelbuscarons@gmail.com>
2023-11-20 16:43:11 -0300

    Modify api URLs

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
-const CATEGORIES_URL = "http://localhost:3000/cats/cat.json";
-const PUBLISH_PRODUCT_URL = "http://localhost:3000/sell/publish.json";
-const PRODUCTS_URL = "http://localhost:3000/cats_products/";
-const PRODUCT_INFO_URL = "http://localhost:3000/products/";
-const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/products_comments/";
-const CART_INFO_URL = "http://localhost:3000/user_cart/";
-const CART_BUY_URL = "http://localhost:3000/cart/buy.json";
+const CATEGORIES_URL = "http://localhost:3000/api/cats/cat.json";
+const PUBLISH_PRODUCT_URL = "http://localhost:3000/api/sell/publish.json";
+const PRODUCTS_URL = "http://localhost:3000/api/cats_products/";
+const PRODUCT_INFO_URL = "http://localhost:3000/api/products/";
+const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/api/products_comments/";
+const CART_INFO_URL = "http://localhost:3000/api/user_cart/";
+const CART_BUY_URL = "http://localhost:3000/api/cart/buy.json";
 const EXT_TYPE = ".json";
 
 if (!localStorage.getItem("autenticado")) {
